939d4e9995404f128a13e2e6177fb19cfc2fc41d
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-19 05:43:35 +0900

    로그인 결과 화면 생성

---
+++ onboardingProject/Sources/LoginResult/LoginResultController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginResultController.swift
+//  onboardingProject
+//
+//  Created by 김윤홍 on 1/19/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class LoginResultController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
