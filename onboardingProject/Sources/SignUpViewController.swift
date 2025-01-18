c377c648ee4e40344e01a45b0c2294cc25639e7e
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-18 20:55:30 +0900

    회원가입 화면 그리기

--- onboardingProject/Sources/SignUpViewController.swift
+++ onboardingProject/Sources/SignUpViewController.swift
@@ -14,10 +14,134 @@ import Then
 
 
 final class SignUpViewController: UIViewController {
- 
+    
+    private let userNameLabel = UILabel().then { label in
+        label.text = "사용자 이름"
+    }
+    
+    private let userNameTextField = UITextField().then { textField in
+        textField.borderStyle = .line
+        textField.backgroundColor = .systemBackground
+    }
+    
+    private let emailLabel = UILabel().then { label in
+        label.text = "이메일"
+    }
+    
+    private let emailTextField = UITextField().then { textField in
+        textField.borderStyle = .line
+    }
+    
+    private let passwordLabel = UILabel().then { label in
+        label.text = "비밀번호"
+    }
+    
+    private let passwordTextField = UITextField().then { textField in
+        textField.borderStyle = .line
+    }
+    
+    private let passwordCheckLabel = UILabel().then { label in
+        label.text = "비밀번호 확인"
+    }
+    
+    private let passwordCheckTextField = UITextField().then { textField in
+        textField.borderStyle = .line
+    }
+    
+    private let signUpButton = UIButton().then { button in
+        button.setTitle("가입하기", for: .normal)
+        button.backgroundColor = .blue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        configureNavigation()
+        configureUI()
+        bindUI()
+    }
+    
+    private func configureNavigation() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationItem.title = "회원가입"
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+        [
+            userNameLabel,
+            emailLabel,
+            passwordLabel,
+            passwordCheckLabel,
+            userNameTextField,
+            emailTextField,
+            passwordTextField,
+            passwordCheckTextField,
+            signUpButton
+        ].forEach { view.addSubview($0) }
+        
+        userNameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+        }
+        
+        userNameTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(userNameLabel.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+
+        emailLabel.snp.makeConstraints { make in
+            make.leading.equalTo(userNameLabel)
+            make.top.equalTo(userNameTextField.snp.bottom).offset(25)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(emailLabel.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        passwordLabel.snp.makeConstraints { make in
+            make.leading.equalTo(userNameLabel)
+            make.top.equalTo(emailTextField.snp.bottom).offset(25)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(passwordLabel.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        passwordCheckLabel.snp.makeConstraints { make in
+            make.leading.equalTo(userNameLabel)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(25)
+        }
+        
+        passwordCheckTextField.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(passwordCheckLabel.snp.bottom).offset(10)
+            make.height.equalTo(40)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(30)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(45)
+        }
     }
 }
+
+extension SignUpViewController {
+    
+    private func bindUI() {
+        
+    }
+}
+
+#Preview {
+    SignUpViewController()
+}
+
