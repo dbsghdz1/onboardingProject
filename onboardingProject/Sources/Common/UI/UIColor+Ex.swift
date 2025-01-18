6c4690d9cc47ff59c81aebd9482fa222d84f23e0
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-18 21:17:09 +0900

    color 익스텐션추가

---
+++ onboardingProject/Sources/Common/UI/UIColor+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Ex.swift
+//  onboardingProject
+//
+//  Created by 김윤홍 on 1/18/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let buttonColor = UIColor(red: 0/255, green: 123/255, blue: 255/255, alpha: 1.0)
+    static let customGrayColor = UIColor(red: 239/250, green: 239/250, blue: 239/250, alpha: 1.0)
+    static let textFieldBorderColor = UIColor(red: 190/250, green: 190/250, blue: 190/250, alpha: 1.0)
+}
