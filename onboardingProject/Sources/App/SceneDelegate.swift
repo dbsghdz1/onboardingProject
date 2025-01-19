88965f0cf04876e980821d9e482696c58cc6d45c
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-19 20:38:45 +0900

    메인 화면 수정

--- onboardingProject/Sources/App/SceneDelegate.swift
+++ onboardingProject/Sources/App/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
  
-        let viewController = UINavigationController(rootViewController: LoginViewController())
+        let viewController = UINavigationController(rootViewController: MainScreen())
         
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
